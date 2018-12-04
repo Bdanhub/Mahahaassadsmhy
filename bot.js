08b97fcafa0b0dd50093553bd55563a359908830
ACTIOMETOPMY <45082436+Bdanhub@users.noreply.github.com>
2018-12-04 07:59:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,37 @@
+const Discord = require('discord.js');
+const tpoints = {};
+const vpoints = {};
+const jimp = require("jimp");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setActivity("",{type: 'watching'})
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
 client.on("message", message => {
 	var prefix = "*";
  if (message.content === "*help") {
@@ -429,3 +463,7 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('لي�
 
 
 });
+
+
+client.login(process.env.BOT_TOKEN);
+
