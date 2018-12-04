7a1aa373faec585884520613d4af32e839a3c4db
ACTIOMETOPMY <45082436+Bdanhub@users.noreply.github.com>
2018-12-04 08:09:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'M'
+const prefix = '*'
 
 client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`By : *help`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start Time] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as [${client.user.username}]`);
-  console.log('')
-  console.log('info:')
-  console.log(`Servers! ["${client.guilds.size}"]`);
-  console.log(`Users! ["${client.users.size}"]`);
-  console.log(`Channels! ["${client.channels.size}"]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('╔[════════════]╗')
-  console.log('ARE YOU READY?')
-  console.log('LETS GO BOT ONLINE!')
-  console.log('╚[════════════]╝')
-  console.log('')
-});
-
-
-client.on('ready', () => {                           
-client.user.setGame(``,'https://www.twitch.tv/*help');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`Mhelp | Minv | ${client.guilds.size} Server`,'https://www.twitch.tv/v5bz');
-}); 
+  client.user.setGame(` *help `);
+}); // نهايه الكود
 
 
 
