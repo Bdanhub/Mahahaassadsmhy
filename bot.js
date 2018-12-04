54098cc3e4a4422ec53b94a07d2dc56a4e086d5e
ACTIOMETOPMY <45082436+Bdanhub@users.noreply.github.com>
2018-12-04 08:11:54 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
 
 client.on("message", message => {
 	var prefix = "*";
- if (message.content === "help") {
+ if (message.content === "*help") {
   const embed = new Discord.RichEmbed()  
       .setColor("#000000") 
       .setDescription(`
